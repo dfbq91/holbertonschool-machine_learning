037c21274efc2227b6950a18b55c974c36361c94
dfbq91 <dfbq91@gmail.com>
2025-04-02 22:01:47 -0500

    📦 feat: tasks 0 to 3

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Calculates the shape of a matrix"""
+
+def matrix_shape(matrix):
+    """Calculates the shape of a matrix"""
+    result = []
+    while (type(matrix) is list):
+        result.append(len(matrix))
+        matrix = matrix[0]
+    return result
