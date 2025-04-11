cd9e0c5b1048a6a2f675d179a1029c2ee3b03cde
dfbq91 <dfbq91@gmail.com>
2025-04-10 20:50:24 -0500

    🧹 chore: fruits

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Module to plot a bar graph with random data."""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bars():
+    """Plot a bar graph with random data."""
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4,3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    t = np.arange(3)
+    bar1 = plt.bar(t, fruit[0], 0.5, color='red')
+    bar2 = plt.bar(t, fruit[1], 0.5, color='yellow', bottom=fruit[0])
+    bar3 = plt.bar(t, fruit[2], 0.5, color='#ff8000', bottom=fruit[0]+fruit[1])
+    bar4 = plt.bar(t, fruit[3], 0.5, color='#ffe5b4',
+                   bottom=fruit[0]+fruit[1]+fruit[2])
+
+    plt.ylabel('Quantity of Fruit')
+    plt.title('Number of Fruit per Person')
+    plt.xticks(t, ('Farrah', 'Fred', 'Felicia'))
+    plt.yticks([i for i in range(0, 90, 10)])
+    plt.legend((bar1, bar2, bar3, bar4), ('apples', 'bananas', 'oranges', 'peaches'))
+    plt.tight_layout()
+    plt.show()
