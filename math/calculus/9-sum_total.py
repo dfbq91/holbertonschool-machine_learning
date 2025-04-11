a2807df755209c8a8bd38cea79729c05c9da3afd
dfbq91 <dfbq91@gmail.com>
2025-04-11 17:59:20 -0500

    🧹 chore: update case

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,6 +6,6 @@ def summation_i_squared(n):
     """Summation of i^2 from 1 to n."""
     if n == 1:
         return 1
-    if type(n) != int:
+    if type(n) != int or n < 1:
         return None
     return summation_i_squared(n - 1) + n ** 2 
