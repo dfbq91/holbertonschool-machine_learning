21c22f86ed29b771be5b11755e7a3820b20666d2
dfbq91 <dfbq91@gmail.com>
2025-04-10 20:56:11 -0500

    🧹 chore: update doc

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -13,15 +13,19 @@ def bars():
     plt.figure(figsize=(6.4, 4.8))
     t = np.arange(3)
     bar1 = plt.bar(t, fruit[0], 0.5, color='red')
-    bar2 = plt.bar(t, fruit[1], 0.5, color='yellow', bottom=fruit[0])
-    bar3 = plt.bar(t, fruit[2], 0.5, color='#ff8000', bottom=fruit[0]+fruit[1])
-    bar4 = plt.bar(t, fruit[3], 0.5, color='#ffe5b4', bottom=fruit[0]+fruit[1]+fruit[2])
+    bar2 = plt.bar(t, fruit[1], 0.5,
+                   color='yellow', bottom=fruit[0])
+    bar3 = plt.bar(t, fruit[2], 0.5, color='#ff8000',
+                   bottom=fruit[0]+fruit[1])
+    bar4 = plt.bar(t, fruit[3], 0.5, color='#ffe5b4',
+                   bottom=fruit[0]+fruit[1]+fruit[2])
     plt.ylabel('Quantity of Fruit')
     plt.title('Number of Fruit per Person')
     plt.xticks(t, ('Farrah', 'Fred', 'Felicia'))
     plt.yticks(np.arange(0, 81, 10))
     plt.ylim(0, 80)
-    plt.legend((bar1, bar2, bar3, bar4), ('apples', 'bananas', 'oranges', 'peaches'))
+    plt.legend((bar1, bar2, bar3, bar4),
+               ('apples', 'bananas', 'oranges', 'peaches'))
     plt.tight_layout()
     plt.show()
 bars()
