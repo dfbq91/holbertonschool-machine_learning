5c518893d36df729ee287d0ef133bf7d9fcdf097
dfbq91 <dfbq91@gmail.com>
2025-04-11 17:52:16 -0500

    🧹 chore: sum total

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""sum_total"""
+
+
+def summation_i_squared(n):
+    """Summation of i^2 from 1 to n."""
+    if n == 1:
+        return 1
+    if type(n) != int:
+        return None
+    return summation_i_squared(n - 1) + n ** 2 
