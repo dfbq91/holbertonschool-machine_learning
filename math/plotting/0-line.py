7a60992d4f349f96c05d0405f9b28c283a89b976
dfbq91 <dfbq91@gmail.com>
2025-04-10 20:21:14 -0500

    🧹 chore: update

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -11,6 +11,6 @@ def line():
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(y, color="r-")
-    plt.xlim(0, 10)
+    plt.plot(y, color='red')
+    plt.axis([0, 10, None, None])
     plt.show()
