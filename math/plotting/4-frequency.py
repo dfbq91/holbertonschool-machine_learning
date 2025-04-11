c22bd2aaa163b10d7bb3e0a1557af271d6ad07a3
dfbq91 <dfbq91@gmail.com>
2025-04-10 20:13:51 -0500

    🧹 chore: updat

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -12,9 +12,8 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(student_grades, edgecolor='black',
-            bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
-    plt.xticks(ticks=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+    plt.hist(student_grades, edgecolor='black', bins=range(0, 101, 10))
+    plt.xticks(ticks=range(0, 101, 10))
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
