45b042c1bf392530f24f9b800627c6ddb1240c8e
dfbq91 <dfbq91@gmail.com>
2025-04-12 13:19:32 -0500

    🧹 chore: is not

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """Summation of i^2 from 1 to n."""
-    if type(n) != int or n < 1:
+    if type(n) is not int or n < 1:
         return None
     return sum(map(lambda i: i**2, range(1, n+1)))
