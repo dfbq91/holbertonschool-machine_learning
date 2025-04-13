2505ece883c43ad0d76f2fc403bb8367154b213c
dfbq91 <dfbq91@gmail.com>
2025-04-13 15:45:50 -0500

    🧹 chore: more tasks

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""10-poly"""
+
+
+def poly_derivative(poly):
+    """Calculates the derivative of a polynomial.
+
+    Args:
+        poly (list): A list of coefficients representing a polynomial.
+
+    Returns:
+        list: A list of coefficients representing the derivative of the polynomial.
+    """
+    result = []
+    if type(poly) is not list or len(poly) == 0:
+        return None
+    if len(poly) == 1:
+        return [0]
+    for i in range(0, len(poly)):
+        if i == 0:
+            continue
+        else:
+            result.append(poly[i] * i)
+    if all(c == 0 for c in result):
+        return [0]
+    return result
+
+print(poly_derivative([5,3,0,1]))
